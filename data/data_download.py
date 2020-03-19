ffbe1e6eb39a650a2656865a0ffec46542470111
Winston Venderbush <winstonvenderbush@gmail.com>
2020-03-19 16:28:56 -0400

    adding data downloader script

---
+++ data/data_download.py
@@ -0,0 +1,30 @@
+import csv
+import requests
+import shutil
+
+
+# with open('catalog.csv') as csv_file:
+#     csv_reader = csv.reader(csv_file, delimiter=',')
+#     line_count = 0
+#     for row in csv_reader:
+#         if line_count == 0:
+#             print(f'Column names are {", ".join(row)}')
+#             line_count += 1
+#         else:
+#             print(f'\t{row[0]} works in the {row[1]} department, and was born in {row[2]}.')
+#             line_count += 1
+#     print(f'Processed {line_count} lines.')
+
+with open('catalog.csv', mode='r', encoding="latin-1") as csv_file:
+	csv_reader = csv.DictReader(csv_file)
+	line_count = 0
+	for row in csv_reader:
+		print(row["URL"])
+		if line_count == 0:
+			print(f'Column names are {", ".join(row)}')
+			line_count += 1
+		#print(f'\t{row["name"]} works in the {row["department"]} department, and was born in {row["birthday month"]}.')
+		line_count += 1
+	print(f'Processed {line_count} lines.')
+
+
