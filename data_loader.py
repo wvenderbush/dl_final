353153b6318e2ac0ab61efa904209744d81c6d67
Winston Venderbush <winstonvenderbush@gmail.com>
2020-04-01 17:20:16 -0400

    adding data statistics

--- data_loader.py
+++ data_loader.py
@@ -3,6 +3,7 @@ import requests
 import shutil
 import time
 import torch
+import statistics as stats
 import numpy as np
 from pathlib import Path
 from os import path
@@ -12,11 +13,13 @@ import pandas as pd
 import matplotlib as plt
 from torch.utils.data import Dataset
 from torchvision import datasets, transforms
+import matplotlib.pyplot as plt; plt.rcdefaults()
+import matplotlib.pyplot as plt
 
 
 class PaintingDataset(Dataset):
 	def __init__(self, transform=None):
-		self.dataframe = grab_frame('data_clean_full.csv')
+		self.dataframe = grab_frame('data/data_clean_full.csv')
 		self.transform = transform
 		self.to_tensor = ToTensor()
 		self.to_pil = ToPILImage()
@@ -34,7 +37,6 @@ class PaintingDataset(Dataset):
 		img_tensor = self.to_tensor(img_obj)
 		img_label = self.labels[index]
 
-
 		return (img_tensor, img_label)
 
 
@@ -43,10 +45,37 @@ def grab_frame(frame_path):
 	return dataset
 
 
+def exp_stats(frame):
+	frame.describe()
+	times = frame["TIMELINE"]
+	t_set = set(times)
+	t_dict = {}
+	print(t_set)
+	for i in t_set:
+		t_dict[i] = 0
+	aps = frame["PATH"]
+	t_w = []
+	t_h = []
+	for p, t in zip(aps, times):
+		image = Image.open("data/" + p)
+		width, height = image.size
+		t_w.append(width)
+		t_h.append(height)
+		t_dict[t] += 1
+		image.close()
+	print("Median Width: " + str(stats.median(t_w)))
+	print("Median Height: " + str(stats.median(t_h)))
+	for i in sorted(t_set):
+		print(str(i) + ": " + str(t_dict[i]))
+
+
+
+
 if __name__ == '__main__':
 	dataset = PaintingDataset()
-	print(len(dataset))
-	print(dataset[20])
+	#print(len(dataset))
+	#print(dataset[20])
+	#exp_stats(grab_frame('data/data_clean_full.csv'))
 	#print(grab_labels())
 
 
